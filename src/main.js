7c013ad6415046dcbab43d58dcb1cb48abdfdf2f
wesseln1 <nwessel429@gmail.com>
2018-11-08 14:12:07 -0600

    finished

--- src/main.js
+++ src/main.js
@@ -14,8 +14,6 @@ planets.forEach((planet) => {
     to a section element in your HTML with an id of "planets".
     Use string templates to construct the DOM elements.
 */
-console.log(newArray)
-planetsEl.innerHTML += newArray
 /*
 Use the map method to create a new array where the
 first letter of each planet is capitalized. Use the
@@ -23,10 +21,12 @@ first letter of each planet is capitalized. Use the
 
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 */
-        let newArray = planets.map(function(x){
-            return x.charAt(0).toUpperCase()+ x.slice(1)
-            
-            
+let newArray = planets.map(function(x){
+    return x.charAt(0).toUpperCase()+ x.slice(1)
+})
+
+console.log(newArray)
+planetsEl.innerHTML += newArray
             /*
             Use the filter method to create a new array that
             contains planets with the letter 'e'. Use the `includes()`
@@ -34,16 +34,24 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
             
             https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 */
-})
-letterEArray = []
-nonEArray = []
-planets.forEach((x) => {
-if( x.includes("e")){
-letterEArray.push(x)
-} else (nonEArray.push(x))
-})
-console.log(letterEArray)
+// })
+// letterEArray = []
+// nonEArray = []
+// planets.forEach((x) => {
+// if( x.includes("e")){
+// letterEArray.push(x)
+// } else (nonEArray.push(x))
+// })
 
+const planetArray = planets.filter(planet => planet.includes("e"))
+console.log(planetArray)
+// planetsEl.innerHTML += planetArray
 
 // Use the reduce method to create a sentence from the words in the following array
 const words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"]
+
+let newWord = words.reduce((previousWord, currenWord) => 
+    `${previousWord} ${currenWord}`)
+    
+console.log(newWord)
+
